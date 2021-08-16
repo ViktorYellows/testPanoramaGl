53b6883b1e22e29b5af5136f982b95d4f202140b
Viktor <korchma98@gmail.com>
2021-08-16 12:15:28 +0300

    initial commit

--- app/src/main/java/com/app/panoramagltestapplication/MainActivity.kt
+++ app/src/main/java/com/app/panoramagltestapplication/MainActivity.kt
@@ -1,9 +1,8 @@
 package com.app.panoramagltestapplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MotionEvent
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.app.panoramagltestapplication.databinding.ActivityMainBinding
 import com.panoramagl.PLImage
 import com.panoramagl.PLManager
@@ -56,6 +55,9 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun changePanorama(index: Int) {
+        // by switching panoramas many times You can catch OOM error, because usage of RAM is increased and not cleared when changing panoramas,
+        // i think that problem is in 65 line (when setting panorama to pLManager), because when i remove it there is no OOM error, but panoramas don't show
+
         if (currentIndex == index)
             return
         val image3D = PLUtils.getBitmap(this, resourceIds[index])
